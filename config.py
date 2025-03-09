cd9c5202ed69da4fc9dcd43342d46a54be88f382
KRISHNA <154213033+krishnabhai99@users.noreply.github.com>
2025-03-09 10:02:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ group = environ.get('GROUP', '')
 GROUP = int(group) if group and id_pattern.search(group) else None
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '5446367898'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8080"))
 LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
